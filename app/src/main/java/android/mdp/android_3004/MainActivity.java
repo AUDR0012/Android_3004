c5e8c8f41d34a1f78cd8aea900a5f13e3c15603e
AUDR0012 <AUDR0012@e.ntu.edu.sg>
2018-09-03 12:51:31 +0800

    menu, timer

--- app/src/main/java/android/mdp/android_3004/MainActivity.java
+++ app/src/main/java/android/mdp/android_3004/MainActivity.java
@@ -3,10 +3,15 @@ package android.mdp.android_3004;
 import android.graphics.Color;
 import android.graphics.PorterDuff;
 import android.graphics.drawable.Drawable;
+import android.os.Handler;
+import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.SwitchCompat;
 import android.view.Gravity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -25,6 +30,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 	ImageView robot;
 	int robot_location = 0;
 
+	Handler handler;
+	long time_start;
+	TextView time_textview;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -34,14 +43,15 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
 //		========== CLICKABLE CONTROLS ==========
 		int[] list_onclick = {R.id.direction_btn_up, R.id.direction_btn_down, R.id.direction_btn_left, R.id.direction_btn_right,
-				R.id.time_btn_timer, R.id.time_btn_reset, R.id.time_swt_isfast,
-				R.id.point_btn_start, R.id.point_btn_way,
-				R.id.display_swt_isauto, R.id.display_btn_update};
+				R.id.time_btn_stopwatch, R.id.time_btn_reset, R.id.time_swt_isfast,
+				R.id.point_btn_origin, R.id.point_btn_way,
+				R.id.config_btn_f1, R.id.config_btn_f2, R.id.config_btn_reconfig,
+				R.id.display_swt_ismanual, R.id.display_btn_update};
 		for (int onclick : list_onclick) {
 			findViewById(onclick).setOnClickListener(this);
 		}
-		onclick_time_option();
-		onclick_display_option();
+		time_option();
+		display_option();
 
 //		========== MAZE ==========
 		grid_maze = findViewById(R.id.grid_maze);
@@ -50,16 +60,18 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
 		Drawable d_box = this.getResources().getDrawable(R.drawable.d_box, null);
 		d_box.setColorFilter(list_color[new Random().nextInt(list_color.length)], PorterDuff.Mode.ADD); //TODO: REMOVABLE
-
 		for (int i = 0; i < MAZE_C * MAZE_R; i++) {
 			TextView tv = new TextView(this);
 			tv.setGravity(Gravity.CENTER);
 			tv.setBackground(d_box);
-			tv.setText(Integer.toString(i));
+			tv.setText(String.valueOf(i));
 
 			grid_maze.addView(tv);
 		}
 		robot_create();
+
+//		========== STOPWATCH ==========
+		handler = new Handler();
 
 //		========== SET POINTS ==========
 		((EditText) findViewById(R.id.point_txt_x)).setHint(Integer.toString(MAZE_C));
@@ -70,7 +82,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
 	@Override
 	public void onClick(View v) {
+		String title; //TODO: REMOVABLE
+		if (v instanceof Button) title = ((Button) v).getText().toString();
+		else title = ((SwitchCompat) v).getText().toString();
+		((TextView) findViewById(R.id.txt_status)).setText(title + " selected");
+
 		switch (v.getId()) {
+			//MAZE
 			case R.id.direction_btn_up:
 				robot_move();
 				break;
@@ -83,27 +101,64 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 			case R.id.direction_btn_right:
 				robot_rotate(1);
 				break;
+
+			//STOPWATCH
 			case R.id.time_swt_isfast:
-				onclick_time_option();
+				time_option();
 				break;
-			case R.id.time_btn_timer:
-				onclick_time_timer(v);
+			case R.id.time_btn_stopwatch:
+				time_stopwatch(v);
 				break;
 			case R.id.time_btn_reset:
-				onclick_time_reset();
+				time_reset();
 				break;
-			case R.id.point_btn_start:
-				onclick_point_set(true);
+
+			//SET POINT
+			case R.id.point_btn_origin:
+				point_set(true);
 				break;
 			case R.id.point_btn_way:
-				onclick_point_set(false);
+				point_set(false);
 				break;
-			case R.id.display_swt_isauto:
-				onclick_display_option();
+
+			//CONFIGURATIONS //TODO: UNKNOWN - WHAT TO DO?
+			case R.id.config_btn_f1:
+				break;
+			case R.id.config_btn_f2:
+				break;
+			case R.id.config_btn_reconfig:
+				break;
+
+			//DISPLAY GRAPHICS
+			case R.id.display_swt_ismanual:
+				display_option();
 				break;
 			case R.id.display_btn_update:
-				onclick_display_manual();
+				display_manual();
 				break;
+		}
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		MenuInflater mi = getMenuInflater();
+		mi.inflate(R.menu.menu, menu);
+		return super.onCreateOptionsMenu(menu);
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) { //TODO: UNKNOWN - MERGE WITH WEEKIAT PART
+		String title = item.getTitle().toString();
+		((TextView) findViewById(R.id.txt_status)).setText(title + " selected");
+		switch (item.getItemId()) {
+			case R.id.menu_bluetooth:
+				return true;
+			case R.id.menu_messenger:
+				return true;
+			case R.id.menu_maze:
+				return true;
+			default:
+				return super.onOptionsItemSelected(item);
 		}
 	}
 
@@ -151,49 +206,82 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 		}
 	}
 
-	protected void onclick_time_option() {
+	protected void time_option() {
 		SwitchCompat s = findViewById(R.id.time_swt_isfast);
 		if (s.isChecked()) {
 			s.setText("Fastest");
+			time_textview = findViewById(R.id.time_txt_fast);
 		} else {
 			s.setText("Explore");
+			time_textview = findViewById(R.id.time_txt_explore);
 		}
+		findViewById(R.id.time_btn_stopwatch).setEnabled(("0:00:000").equalsIgnoreCase(time_textview.getText().toString()));
 	}
 
-	protected void onclick_time_timer(View v) { //TODO: TIMER ITSELF
+	protected void time_stopwatch(View v) {
 		Button b = (Button) v;
 		if (("Start").equalsIgnoreCase(b.getText().toString())) {
-			b.setText("Stop");
+			time_start = SystemClock.uptimeMillis();
+			handler.postDelayed(runnable, 0);
+
+			findViewById(R.id.time_swt_isfast).setEnabled(false);
+			findViewById(R.id.time_btn_reset).setEnabled(false);
+			b.setText("Pause");
 		} else {
+			handler.removeCallbacks(runnable);
+
+			findViewById(R.id.time_swt_isfast).setEnabled(true);
+			findViewById(R.id.time_btn_reset).setEnabled(true);
+			findViewById(R.id.time_btn_stopwatch).setEnabled(false);
 			b.setText("Start");
 		}
 	}
 
-	protected void onclick_time_reset() { //TODO: CLEAR TEXT
-		SwitchCompat s = findViewById(R.id.time_swt_isfast);
-		TextView tv = (TextView) (s.isChecked() ? findViewById(R.id.time_txt_fast) : findViewById(R.id.time_txt_explore));
-		tv.setText("00.00.00");
+	protected void time_reset() {
+		time_start = 0L;
+		time_set(0, 0, 0);
+		findViewById(R.id.time_btn_stopwatch).setEnabled(true);
 	}
 
-	protected void onclick_point_set(boolean isstart) { //TODO: SETTING POINTS
-		int x = Integer.valueOf(((EditText) findViewById(R.id.point_txt_x)).getText().toString()),
-				y = Integer.valueOf(((EditText) findViewById(R.id.point_txt_y)).getText().toString());
-		//if (isstart &&)
+	protected void time_set(int min, int sec, int millisec) {
+		time_textview.setText("" + min + ":"
+				+ String.format("%02d", sec) + ":"
+				+ String.format("%03d", millisec));
 	}
 
-	protected void onclick_display_option() {
-		SwitchCompat s = findViewById(R.id.display_swt_isauto);
+	public Runnable runnable = new Runnable() {
+		public void run() {
+			long time_count_ms = SystemClock.uptimeMillis() - time_start;
+			int time_s = (int) (time_count_ms / 1000);
+
+			time_set(time_s / 60, time_s % 60, (int) (time_count_ms % 1000));
+			handler.postDelayed(this, 0);
+		}
+
+	};
+
+	protected void point_set(boolean isorigin) { //TODO: SETTING POINTS
+//		int cell,
+//		point_x = Integer.valueOf(((TextView)findViewById(R.id.point_txt_x));
+//		if (isorigin) {
+//
+//		}else{
+//		}
+	}
+
+	protected void display_option() { //TODO: HOW TO AUTO?
+		SwitchCompat s = findViewById(R.id.display_swt_ismanual);
 		Button b = findViewById(R.id.display_btn_update);
 		if (s.isChecked()) {
-			s.setText("Auto");
-			b.setVisibility(View.INVISIBLE);
-		} else {
 			s.setText("Manual");
 			b.setVisibility(View.VISIBLE);
+		} else {
+			s.setText("Auto");
+			b.setVisibility(View.INVISIBLE);
 		}
 	}
 
-	protected void onclick_display_manual() { //TODO: UPDATE MANUAL
+	protected void display_manual() { //TODO: UNKNOWN - UPDATE MANUAL
 
 	}
 }
