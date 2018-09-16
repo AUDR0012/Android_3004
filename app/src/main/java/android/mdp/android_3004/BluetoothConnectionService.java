e0cce222cd7e8afbf0d4db0a957771ac7d553602
AUDR0012 <AUDR0012@e.ntu.edu.sg>
2018-09-16 14:50:50 +0800

    Revert "weekiat bt"
    This reverts commit 8cac47defe01d40a8bf65d20990f4aee4f1f2428.

--- app/src/main/java/android/mdp/android_3004/BluetoothConnectionService.java
+++ app/src/main/java/android/mdp/android_3004/BluetoothConnectionService.java
@@ -1,139 +1,39 @@
 package android.mdp.android_3004;
 
+import android.app.ProgressDialog;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothServerSocket;
 import android.bluetooth.BluetoothSocket;
 import android.content.Context;
-import android.os.Bundle;
-import android.os.Handler;
-import android.os.Message;
+import android.content.Intent;
+import android.support.v4.content.LocalBroadcastManager;
+import android.util.Log;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.Stack;
 import java.util.UUID;
 
 public class BluetoothConnectionService {
-
-	private final UUID THIS_UUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
-	private final BluetoothAdapter bt_adapter;
-	private final Handler bt_handler;
+	private final UUID BT_UUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
+	private BluetoothAdapter bt_adapter;
 	private Context context;
 
 	private AcceptThread btt_accept;
+
 	private ConnectThread btt_connect;
+	private BluetoothDevice bt_device;
+	private UUID bt_device_UUID;
+
 	private ConnectedThread btt_connected;
 
-	private Enum.State state_cur;
-
-	BluetoothConnectionService(Context context, Handler handler) {
+	BluetoothConnectionService(Context context) {
 		this.context = context;
-		this.bt_handler = handler;
 		this.bt_adapter = BluetoothAdapter.getDefaultAdapter();
-
-		state_cur = Enum.State.NONE;
-	}
-
-	private synchronized void updateTitle() {
-		bt_handler.obtainMessage(Enum.Handling.STATE_CHANGE.get(), state_cur.get(), -1).sendToTarget();
-	}
-
-	public synchronized void start() {
-		if (btt_connect != null) {
-			btt_connect.cancel();
-			btt_connect = null;
-		}
-		if (btt_connected != null) {
-			btt_connected.cancel();
-			btt_connected = null;
-		}
-		if (btt_accept == null) {
-			btt_accept = new AcceptThread();
-			btt_accept.start();
-		}
-
-		updateTitle();
-	}
-
-	public synchronized void connect(BluetoothDevice device) {
-		if (state_cur == Enum.State.CONNECTING && btt_connect != null) {
-			btt_connect.cancel();
-			btt_connect = null;
-		}
-		if (btt_connected != null) {
-			btt_connected.cancel();
-			btt_connected = null;
-		}
-
-		btt_connect = new ConnectThread(device);
-		btt_connect.start();
-		updateTitle();
-	}
-
-	private synchronized void connected(BluetoothSocket socket, BluetoothDevice device) {
-		if (btt_connect != null) {
-			btt_connect.cancel();
-			btt_connect = null;
-		}
-		if (btt_connected != null) {
-			btt_connected.cancel();
-			btt_connected = null;
-		}
-		if (btt_accept == null) {
-			btt_accept.cancel();
-			btt_accept.start();
-		}
-
-		btt_connected = new ConnectedThread(socket);
-		btt_connected.start();
-
-		Message msg = bt_handler.obtainMessage(Enum.Handling.BT_NAME.get());
-		Bundle bundle = new Bundle();
-		bundle.putString(Enum.Handling.BT_NAME.getDesc(), device.getName());
-		msg.setData(bundle);
-		bt_handler.sendMessage(msg);
-
-		updateTitle();
-	}
-
-	public synchronized void stop() {
-		if (btt_connect != null) {
-			btt_connect.cancel();
-			btt_connect = null;
-		}
-		if (btt_connected != null) {
-			btt_connected.cancel();
-			btt_connected = null;
-		}
-		if (btt_accept == null) {
-			btt_accept.cancel();
-			btt_accept = null;
-		}
-
-		state_cur = Enum.State.NONE;
-		updateTitle();
-	}
-
-	public void write(byte[] message_out) {
-		ConnectedThread r;
-		synchronized (this) {
-			if (state_cur != Enum.State.CONNECTED) return;
-			r = btt_connected;
-		}
-		r.write(message_out);
-	}
-
-	private void failed(boolean hasfailed) {
-		Message msg = bt_handler.obtainMessage(Enum.Handling.TOAST.get());
-		Bundle bundle = new Bundle();
-		bundle.putString(Enum.Handling.TOAST.getDesc(), hasfailed ? "Unable to connect device" : "Device connection was lost");
-		msg.setData(bundle);
-		bt_handler.sendMessage(msg);
-
-		state_cur = Enum.State.NONE;
-
-		updateTitle();
 		start();
 	}
 
@@ -144,47 +44,27 @@ public class BluetoothConnectionService {
 			BluetoothServerSocket tmp = null;
 
 			try {
-				tmp = bt_adapter.listenUsingInsecureRfcommWithServiceRecord(context.getString(R.string.app_name), THIS_UUID);
+				tmp = bt_adapter.listenUsingInsecureRfcommWithServiceRecord(context.getString(R.string.app_name), BT_UUID);
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
 			server_socket = tmp;
-			state_cur = Enum.State.LISTEN;
 		}
 
 		public void run() {
 			BluetoothSocket socket = null;
+			try {
+				socket = server_socket.accept();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
 
-			while (state_cur != Enum.State.CONNECTED) {
-				try {
-					socket = server_socket.accept();
-				} catch (IOException e) {
-					e.printStackTrace();
-					break;
-				}
-
-				if (socket != null) {
-					synchronized (this) {
-						switch (state_cur) {
-							case LISTEN:
-							case CONNECTING:
-								connected(socket, socket.getRemoteDevice());
-								break;
-							case NONE:
-							case CONNECTED:
-								try {
-									socket.close();
-								} catch (IOException e) {
-									e.printStackTrace();
-								}
-								break;
-						}
-					}
-				}
+			if (socket != null) {
+				connected(socket, bt_device);
 			}
 		}
 
-		private void cancel() {
+		public void cancel() {
 			try {
 				server_socket.close();
 			} catch (IOException e) {
@@ -194,25 +74,23 @@ public class BluetoothConnectionService {
 	}
 
 	private class ConnectThread extends Thread {
-		private final BluetoothSocket socket;
-		private final BluetoothDevice device;
+		BluetoothSocket socket = null;
 
-		ConnectThread(BluetoothDevice bd) {
-			device = bd;
+		ConnectThread(BluetoothDevice bd, UUID uuid) {
+			bt_device = bd;
+			bt_device_UUID = uuid;
+		}
+
+		public void run() {
 			BluetoothSocket tmp = null;
-
 			try {
-				tmp = device.createRfcommSocketToServiceRecord(THIS_UUID);
+				tmp = bt_device.createRfcommSocketToServiceRecord(bt_device_UUID);
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
 			socket = tmp;
-			state_cur = Enum.State.CONNECTING;
-		}
 
-		public void run() {
 			bt_adapter.cancelDiscovery();
-
 			try {
 				socket.connect();
 			} catch (IOException e) {
@@ -221,18 +99,12 @@ public class BluetoothConnectionService {
 				} catch (IOException e1) {
 					e1.printStackTrace();
 				}
-				failed(true);
-				return;
+				e.printStackTrace();
 			}
-
-			synchronized (this) {
-				btt_connect = null;
-			}
-
-			connected(socket, device);
+			connected(socket, bt_device);
 		}
 
-		private void cancel() {
+		void cancel() {
 			try {
 				socket.close();
 			} catch (IOException e) {
@@ -241,8 +113,24 @@ public class BluetoothConnectionService {
 		}
 	}
 
+	public synchronized void start() {
+		if (btt_connect != null) {
+			btt_connect.cancel();
+			btt_connect = null;
+		}
+		if (btt_accept == null) {
+			btt_accept = new AcceptThread();
+			btt_accept.start();
+		}
+	}
+
+	public void start_client(BluetoothDevice device, UUID device_UUID) {
+		btt_connect = new ConnectThread(device, device_UUID);
+		btt_connect.start();
+	}
+
 	public class ConnectedThread extends Thread {
-		private final BluetoothSocket socket;
+		private BluetoothSocket socket;
 		private final InputStream stream_in;
 		private final OutputStream stream_out;
 
@@ -253,47 +141,73 @@ public class BluetoothConnectionService {
 
 			try {
 				tmp_in = socket.getInputStream();
+			} catch (IOException e) {
+				e.printStackTrace();
+				Log.e("BT-ERROR", "Failed to create input stream");
+			}
+
+			try {
 				tmp_out = socket.getOutputStream();
 			} catch (IOException e) {
 				e.printStackTrace();
+				Log.e("BT-ERROR", "Failed to create output stream");
 			}
+
 			stream_in = tmp_in;
 			stream_out = tmp_out;
-
-			state_cur = Enum.State.CONNECTED;
 		}
 
 		public void run() {
 			byte[] buffer = new byte[1024];
 			int bytes;
 
-			while (state_cur == Enum.State.CONNECTED) {
+			while (true) {
 				try {
 					bytes = stream_in.read(buffer);
-					bt_handler.obtainMessage(Enum.Handling.READ_MSG.get(), bytes, -1, buffer).sendToTarget();
+					String message = new String(buffer, 0, bytes);
+
+					Intent messaging_intent = new Intent("messaging");
+					messaging_intent.putExtra("read message", message);
+					LocalBroadcastManager.getInstance(context).sendBroadcast(messaging_intent);
 				} catch (IOException e) {
 					e.printStackTrace();
-					failed(false);
 					break;
 				}
 			}
 		}
 
 		void write(byte[] bytes) {
+			Log.d("TMP", "Socket Conn: " + socket.isConnected());
 			try {
+				stream_out.flush();
+				Log.d("TMP", "K flushed");
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			try {
+				//stream_out.write(bytes);
+				Log.d("TMP", "Socket Conn: " + socket.isConnected());
 				stream_out.write(bytes);
-				bt_handler.obtainMessage(Enum.Handling.WRITE_MSG.get(), -1, -1, bytes).sendToTarget();
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
 		}
 
-		private void cancel() {
+		public void cancel() {
 			try {
 				socket.close();
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
 		}
+	}
+
+	private void connected(BluetoothSocket socket, BluetoothDevice device) {
+		btt_connected = new ConnectedThread(socket);
+		btt_connected.start();
+	}
+
+	public void write(byte[] message_out) {
+		btt_connected.write(message_out);
 	}
 }
